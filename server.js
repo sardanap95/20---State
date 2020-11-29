f8711fc8ce31a357820124cc828f3d21231d643d
Pushpi <sardanap95@gmail.com>
2020-11-29 08:51:33 +0530

    Minor

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8080;
 var app = express();
-app.use(express.static(path.join(__dirname + "/build")));
+app.use(express.static(path.join(__dirname, "build/")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
 app.listen(port, () => {
+  const x = path.join(__dirname, "build/");
+  console.log(x);
   console.log("Server running on port " + port);
 });
